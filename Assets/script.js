c70616cfc1c4e4baef0bf1722b9a58e4052b8580
Remmington Pascone <remmingtonpearce@gmail.com>
2021-01-07 20:08:33 -0800

    cleaned up HTML/CSS for styling, still need 5day API

--- Assets/script.js
+++ Assets/script.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
                 $("#currentDay").text(moment().format("dddd, MMMM Do "));
 
                 // Transfer content to HTML
-                $(".city").html("<h1>" + response.name + "</h1>");
+                $(".city").html("<h1>" + response.name  + "</h1>");
                 $(".weatherDesc").html("<h3>" + response.weather[0].main + "</h3>");
                 // with conversion to F and 2 decimal points
                 $(".temp").html("<h4> Temperature: " + (((response.main.temp - 273.15) * 1.80 + 32).toFixed(2)) + "</h4>");
@@ -33,6 +33,9 @@ $(document).ready(function () {
             });
 
         var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=31581a27df415fae48da576f8172e0c6"
+
+        var currentHour = moment().hours();
+        console.log(currentHour);
 
         // Performing our 5-Day AJAX GET request
         $.ajax({
@@ -46,6 +49,39 @@ $(document).ready(function () {
                 // Log the resulting object
                 console.log(response);
 
+                var timeBlock = document.querySelectorAll(".time-block");
+    console.log(timeBlock.length);
+
+    // forEach loop over hour blocks
+    $(".time-block").each(function () {
+        // get value from html for current hour 
+        var blockHour = parseInt($(this).attr("id").split("-")[1]);
+        console.log(blockHour);
+
+
+        // change div timeblock colors depending on time of day
+        // check if currentHour > or < blockHour
+        // add css classes according to time
+        if (currentHour > blockHour) {
+            // testing that my loop is working
+            console.log("div is grey");
+            $(this).addClass("past");
+
+        }
+        else if (currentHour < blockHour) {
+            // testing that my loop is working
+            console.log("div is green");
+            $(this).addClass("future");
+        }
+
+        else {
+            // testing that my loop is working
+            console.log("div is red");
+            $(this).addClass("present");
+
+        }
+
+    });
 
             
             });
