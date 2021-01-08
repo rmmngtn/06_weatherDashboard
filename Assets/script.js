45b1e70b25b6e55cfef8ff8f8e3967aba5b12a46
Remmington Pascone <remmingtonpearce@gmail.com>
2021-01-07 19:29:17 -0800

    successful ajax calls, logs results for 5 day forecast, needs appending based on hour/day

--- Assets/script.js
+++ Assets/script.js
@@ -1,51 +1,70 @@
 $(document).ready(function () {
-    
+
     $("#searchBtn").on("click", function () {
- 
-    var cityName = $("#input").val();
-    // api call to get jumbotron information listed 
-    
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=31581a27df415fae48da576f8172e0c6";
-       
-    // Performing our AJAX GET request
+
+        var cityName = $("#input").val();
+        // api call to get jumbotron information listed 
+
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=31581a27df415fae48da576f8172e0c6";
+
+        // Performing our AJAX GET request
         $.ajax({
             url: queryURL,
             method: "GET"
         })
-        .then(function(response) {
-       // Log the queryURL
-       console.log(queryURL);
+            .then(function (response) {
+                // Log the queryURL
+                console.log(queryURL);
 
-       // Log the resulting object
-       console.log(response);
-        
-       $("#currentDay").text(moment().format("dddd, MMMM Do "));
+                // Log the resulting object
+                console.log(response);
 
-        // Transfer content to HTML
-       $(".city").html("<h1>" + response.name + "</h1>");
-       $(".weatherDesc").html("<h3>" + response.weather[0].main + "</h3>");
-       $(".temp").html("<h4> Temperature: "+ ((response.main.temp - 273.15) * 1.80 + 32)+ "</h4>");
-       $(".humidity").html("<h4> Humidity: " + response.main.humidity + "</h4>");
-       $(".wind-speed").html("<h4> Wind Speed: " + response.wind.speed + "</h4>");
-      
-       
-       
-    //    // Convert the temp to fahrenheit
-    //    var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+                $("#currentDay").text(moment().format("dddd, MMMM Do "));
 
-    //    // add temp content to html
-    //    $(".temp").text("Temperature (K) " + response.main.temp);
-    //    $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
+                // Transfer content to HTML
+                $(".city").html("<h1>" + response.name + "</h1>");
+                $(".weatherDesc").html("<h3>" + response.weather[0].main + "</h3>");
+                // with conversion to F and 2 decimal points
+                $(".temp").html("<h4> Temperature: " + (((response.main.temp - 273.15) * 1.80 + 32).toFixed(2)) + "</h4>");
+                $(".humidity").html("<h4> Humidity: " + response.main.humidity + "</h4>");
+                $(".wind-speed").html("<h4> Wind Speed: " + response.wind.speed + "</h4>");
 
-    //    // Log the data in the console as well
-    //    console.log("Wind Speed: " + response.wind.speed);
-    //    console.log("Humidity: " + response.main.humidity);
-    //    console.log("Temperature (F): " + tempF);
+                //    need additional API for UV index
+            });
+
+        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=31581a27df415fae48da576f8172e0c6"
+
+        // Performing our 5-Day AJAX GET request
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function (response) {
+                // Log the queryURL
+                console.log(queryURL);
+
+                // Log the resulting object
+                console.log(response);
 
 
-        }); 
-    }); 
+            
+            });
+
+
+
+            // Transfer content to HTML
+        //    $(".city").html("<h1>" + response.name + "</h1>");
+        //    $(".weatherDesc").html("<h3>" + response.weather[0].main + "</h3>");
+        //     // with conversion to F and 2 decimal points
+        //    $(".temp").html("<h4> Temperature: "+ (((response.main.temp - 273.15) * 1.80 + 32).toFixed(2)) + "</h4>");
+        //    $(".humidity").html("<h4> Humidity: " + response.main.humidity + "</h4>");
+        //    $(".wind-speed").html("<h4> Wind Speed: " + response.wind.speed + "</h4>");
+
+
+
+
+
+    });
+}); 
 
     
-});
-            
